fa05f2de1c03b8ce6c42210a021c3281bab9e42f
odejinmisa <odejinmisa@newwavesecosystem.com>
2024-04-04 09:10:07 +0100

    -Implement noise suppression

--- src/lib/microphone/requestMicrophoneAccess.ts
+++ src/lib/microphone/requestMicrophoneAccess.ts
@@ -1,7 +1,12 @@
 const requestMicrophoneAccess = async (desiredMic: MediaDeviceInfo|undefined): Promise<MediaStream | null> => {
   try {
     return await navigator.mediaDevices.getUserMedia({
-      audio: {deviceId: {exact: desiredMic?.deviceId}},
+      audio: {
+          deviceId: {exact: desiredMic?.deviceId},
+          autoGainControl: false,
+          noiseSuppression: true,
+          echoCancellation: true
+        },
     });
   } catch (error) {
     return null;
