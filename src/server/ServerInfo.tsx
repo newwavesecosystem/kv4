cc83953806684e4aaccc829dca020216c2defa51
odejinmisa <odejinmisa@newwavesecosystem.com>
2024-04-15 08:40:48 +0100

    Chnage the engine url

--- src/server/ServerInfo.tsx
+++ src/server/ServerInfo.tsx
@@ -1,13 +1,13 @@
 export const laravelAppURL = "https://dev.konn3ct.ng";
 export const extRegisterURL = "https://k40.konn3ct.ng";
-export const engineBaseURL = "devmeet.konn3ct.ng";
+export const engineBaseURL = "meet.konn3ct.ng";
 export const tokenValidationURL = `https://${engineBaseURL}/bigbluebutton/api/enter`;
 export const joinURL = `http://127.0.0.1:8000/api/k4/`;
 export const sfuURL = `wss://${engineBaseURL}/bbb-webrtc-sfu`;
 export const websocketURL = `https://${engineBaseURL}/html5client/sockjs`;
 export const aiBotURL = `${extRegisterURL}/ai-bot-message`;
-// export const aiEnginesURL = `http://34.207.102.15:3131/konn3ctai`;
-export const aiEnginesURL = `https://k4aiengine.konn3ct.ng/konn3ctai`;
+export const aiEnginesURL = `https://kv4-captions.vercel.app/konn3ctai`;
+// export const aiEnginesURL = `https://k4aiengine.konn3ct.ng/konn3ctai`;
 // export const aiEnginesURL = `http://localhost:3002/konn3ctai`;
 
 export function generateRandomId(length:number) {
